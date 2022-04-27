ea4c6921ef1b74b30d6d5131ad8617380fa1a350
Maximus2563 <88945337+Maximus2563@users.noreply.github.com>
2022-04-27 14:53:45 +0100

    Update custom_item_components.py

--- resources/custom_components/custom_item_components.py
+++ resources/custom_components/custom_item_components.py
@@ -18,6 +18,40 @@ class DoNothing(ItemComponent):
 
     expose = Type.Int
     value = 1
+    
+    class BlastAOE(ItemComponent):
+    nid = 'blast_aoe'
+    desc = "Blast extends outwards the specified number of tiles."
+    tag = ItemTags.CUSTOM
+
+    expose = Type.Int  # Radius
+    value = 1
+
+    def _get_power(self, unit) -> int:
+        empowered_splash = skill_system.empower_splash(unit)
+        return self.value + 1 + empowered_splash
+
+    def splash(self, unit, item, position) -> tuple:
+        ranges = set(range(self._get_power(unit)))
+        splash = target_system.find_manhattan_spheres(ranges, position[0], position[1])
+        splash = {pos for pos in splash if game.tilemap.check_bounds(pos)}
+        from app.engine import item_system
+        if item_system.is_spell(unit, item):
+            # spell blast
+            splash = [game.board.get_unit(s) for s in splash]
+            splash = [s.position for s in splash if s]
+            return None, splash
+        else:
+            # regular blast
+            splash = [game.board.get_unit(s) for s in splash if s != position]
+            splash = [s.position for s in splash if s]
+            return position if game.board.get_unit(position) else None, splash
+
+    def splash_positions(self, unit, item, position) -> set:
+        ranges = set(range(self._get_power(unit)))
+        splash = target_system.find_manhattan_spheres(ranges, position[0], position[1])
+        splash = {pos for pos in splash if game.tilemap.check_bounds(pos)}
+        return splash
 
 class RallyBlastAOE(BlastAOE, ItemComponent):
     nid = 'rally_aoe'
